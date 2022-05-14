8e2a5b5b659361618dc09c78caf66ffe30d2f34d
110101 <ideenfront@googlemail.com>
2022-05-14 22:14:00 +0200

    Uodate 05/22

--- website/trailcondition/views.py
+++ website/trailcondition/views.py
@@ -2,8 +2,8 @@ from django.http import HttpResponse, HttpResponseRedirect
 from django.template import loader
 from django.shortcuts import get_object_or_404, render
 from django.urls import reverse
-from algo import run_algo
-from algo import dojo
+import run_algo
+import dojo
 
 # Create your views here.
 # index view
